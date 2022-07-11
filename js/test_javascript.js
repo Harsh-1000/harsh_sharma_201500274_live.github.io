d887415083877e28ef803b16ba61038fbf97849f
Harsh-1000 <harshgoku001@gmail.com>
2022-07-11 23:07:49 +0530

    JOVAC FINAL ASSIGNMENT

---
+++ js/test_javascript.js
@@ -0,0 +1,17 @@
+let name = document.getElementById("name");
+name.innerHTML = "HARSH SHARMA";
+
+let rollno = document.getElementById("rollno");
+rollno.innerHTML = "201500274";
+
+let college = document.getElementById("college");
+college.innerHTML = "GLA UNIVERSITY MATHURA";
+
+let d = new Date();
+
+let date = d.getDate() + " - " + d.getMonth() + " - " + d.getFullYear();
+let da = document.getElementById("date");
+da.innerHTML = date;
+
+
+
